1182c8c9155fd68179fd2be039bd785183d7641f
zkj <438101845@qq.com>
2020-08-10 21:48:41 +0800

    [zkj]-{feat}-[2-array]: finished map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,35 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  return collection.map(number => {
+    const index = (number - 1) % 26;
+    return letters[index];
+  });
 }
