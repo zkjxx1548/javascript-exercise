894b283d40b31ccab8a2eca46bc155f4038cb5f9
zkj <438101845@qq.com>
2020-08-10 22:31:40 +0800

    [zkj]-{feat}-[3-object]: finished values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,10 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
   const collection = Object.values(source);
-  let sum;
-  collection.map(string => parseInt(string, 10));
+  let sum = 0;
   collection.forEach(number => {
-    sum += number;
+    sum += parseInt(number, 10);
   });
   return sum;
 }
