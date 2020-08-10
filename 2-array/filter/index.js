7540b933a7fac41e4afcae07e90a524089ca42fc
zkj <438101845@qq.com>
2020-08-10 21:30:40 +0800

    [zkj]-{feat}-[2-array]: finished filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,17 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(number => number % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = [collection[0]];
+  collection.array.forEach(element => {
+    if (!result.includes(element)) {
+      result.push(element);
+    }
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
