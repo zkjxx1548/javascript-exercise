74082db0ddc8a6ae36b9f3331c38c792e1a3a511
zkj <438101845@qq.com>
2020-08-10 23:21:38 +0800

    [zkj]-{feat}-[4-class-practice3]: finished Person.class and Student.class and Worker.class

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,14 @@
 // TODO 13: 在这里写实现代码
+const { default: Person } = require('./person');
+
+class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.class = klass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}`;
+  }
+}
+export default Student;
--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,10 @@
+const { default: Person } = require('./person');
+
 // TODO 14: 在这里写实现代码
+class Worker extends Person {
+  // eslint-disable-next-line class-methods-use-this
+  introduce() {
+    return 'I am a Worker. I have a job.';
+  }
+}
+export default Worker;
