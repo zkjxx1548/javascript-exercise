d56635450928eb6dbf6d1dc8ac3fa4c5a734b9c7
zkj <438101845@qq.com>
2020-08-11 00:31:17 +0800

    [zkj]-{feat}-[5-async-fetch]: finished

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,19 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  const options = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  fetch(url, options).then(response => {
+    if (response.status >= 200 && response.status <= 299) {
+      Response.success(response);
+    } else {
+      Response.failure(response);
+    }
+  });
   // end -->
 }
 
