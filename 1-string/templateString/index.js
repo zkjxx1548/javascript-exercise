8e3faa42d3de01fd0036a55a092322c6da83928c
zkj <438101845@qq.com>
2020-08-10 21:20:20 +0800

    [zkj]-{feat}-[1-string]: finished templateString

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,5 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  return `你好，${person.lastName}${person.firstName}！你的考试成绩为${getScore(person.id)}分`;
 }
