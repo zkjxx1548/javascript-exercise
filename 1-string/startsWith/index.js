4484147db53e243665cab2939bd49ef8cd8c6e45
zkj <438101845@qq.com>
2020-08-10 21:04:00 +0800

    [zkj]-{feat}-[1-string]: finnished

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let count = 0;
+  collection.forEach(element => {
+    if (element.charAt(0) === '粤') {
+      count += 1;
+    }
+  });
+  return count;
 }
