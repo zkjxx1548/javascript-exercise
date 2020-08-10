4b6a29186116a6acc2e7f79ec18df6e0e5efaa34
zkj <438101845@qq.com>
2020-08-10 22:01:13 +0800

    [zkj]-{feat}-[3-object]: finished assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,11 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const source2 = {
+    serialNumber: '12345',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  return Object.assign(source, source2);
 }
