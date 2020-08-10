cde23817cdfedc461c02fb7e0c785d1485c4103d
zkj <438101845@qq.com>
2020-08-11 00:05:34 +0800

    [zkj]-{feat}-[5-async]: finished

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,18 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('POST', url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  // eslint-disable-next-line func-names
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+      successCallback(xhr);
+    }
+    if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+      errorCallback(xhr);
+    }
+  };
+  xhr.send();
   // end -->
 }
 
