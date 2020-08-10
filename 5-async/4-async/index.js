d6ff6b66ed766f9d5f87768596871355f08d3a86
zkj <438101845@qq.com>
2020-08-11 00:41:53 +0800

    [zkj]-{feat}-[5-async-async]: finished

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,26 @@
+function myFetch(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    // eslint-disable-next-line func-names
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+        resolve(xhr);
+      }
+      if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+        reject(xhr);
+      }
+    };
+    xhr.send();
+  });
+}
+
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const xhr = await myFetch(url);
+  Response.success(xhr);
   // end -->
 }
 
